730bd3931e4f1c46c0735205df3a5452e8133aab
Filippo Tampieri <fxt@fxtlabs.com>
2011-05-30 20:40:49 -0400

    Added/improved docstrings.

--- src/stockings/exchanges.clj
+++ src/stockings/exchanges.clj
@@ -1,5 +1,7 @@
 (ns stockings.exchanges
-  "Functions to get basic info on the AMEX, NASDAQ, and NYSE stock exchanges."
+  "Functions to get a listing of the companies traded on the AMEX,
+   NASDAQ, and NYSE stock exchanges. Based on the data made available
+   by the NASDAQ at http://www.nasdaq.com/screening/company-list.aspx."
   {:author "Filippo Tampieri <fxt@fxtlabs.com>"}
   (:use [clojure.string :only (split lower-case upper-case)]
         [clojure.contrib.def :only (defvar defvar-)]
@@ -13,21 +15,21 @@
 
 (defvar nasdaq
   {:name "NASDAQ Stock Market", :symbol "NASDAQ"}
-  "A StockExchange object describing the NASDAQ Stock Market (NASDAQ).")
+  "A map describing the NASDAQ Stock Market (NASDAQ).")
 
 (defvar nyse
   {:name  "New York Stock Exchange", :symbol "NYSE"}
-  "A StockExchange object describing the New York Stock Exchange (NYSE).")
+  "A map describing the New York Stock Exchange (NYSE).")
 
 (defvar amex
   {:name "NYSE Amex Equities", :symbol "AMEX"}
-  "A StockExchange object describing the NYSE Amex Equities (AMEX).")
+  "A map describing the NYSE Amex Equities (AMEX).")
 
 (defvar exchanges
   {:amex amex,
    :nasdaq nasdaq
    :nyse nyse}
-  "A map from stock exchange keywords to StockExchange objects.")
+  "A map from stock exchange keywords to stock exchange info maps.")
 
 ;;;
 ;;; Industry Sectors
@@ -54,6 +56,8 @@
 
 (defvar- source-url "http://www.nasdaq.com/screening/companies-by-name.aspx")
 
+;; Use a record instead of a map for efficiency since the lists of
+;; companies are pretty long.
 (defrecord Company [exchange symbol name ipo-year sector industry])
 
 (defn- valid-record?
@@ -72,7 +76,7 @@
    It assumes the CSV record is a sequence of strings corresponding to the
    following fields: symbol, name, last sale, market capitalization, IPO year,
    sector, industry, URL for summary quote.
-   The resulting security map retains the exchange, symbol, name, IPO year,
+   The resulting Company record retains the exchange, symbol, name, IPO year,
    sector, and industry for the company."
   [exchange-key r]
   (let [stock-symbol (upper-case (nth r 0))
@@ -85,7 +89,14 @@
 
 (defn parse-companies
   "Parses a string of CSV-encoded companies and returns them
-   as a sequence of Company records."
+   as a sequence of Company records. It expects one company record per
+   line with the following fields: symbol, name, last sale, market
+   capitalization, IPO year, sector, industry, and URL for summary quote.
+   The first line is expected to contain the column headers and is discarded.
+   The first parameter should be a key representing the exchange on which all
+   the companies described in the input string are traded.
+   The result includes exchange, symbol name, IPO year, sector, and
+   industry for each company."
   [exchange-key ^String s]
   (->> s
        parse-csv
@@ -95,9 +106,10 @@
 
 (defn get-companies
   "Requests a list of the companies traded on the stock exchange denoted by
-   the supplied keyword. If not keyword is provided, it returns a merged list
+   the supplied keyword. If no keyword is provided, it returns a merged list
    of the companies traded on the NASDAQ, NYSE, and AMEX exchanges.
-   The companies are returned as a sequence of Company records."
+   The companies are returned as a sequence of Company records.
+   See parse-companies for details."
   ([exchange-key]
      (let [params {:render "download", :exchange (name exchange-key)}
            request (client/get source-url {:query-params params})]
@@ -122,7 +134,7 @@
   "Builds a lookup function corresponding to the supplied list of companies
    (a sequence of Company records). The lookup function takes a stock symbol
    and returns the Company record corresponding to it. The stock symbol may
-   optionally be qualified with a stock exchange (e.g. \"NASDAQ:GOOG\").
+   optionally be prefixed with a stock exchange (e.g. \"NASDAQ:GOOG\").
    If the stock exchange is not specified and multiple matches are found, it
    returns a vector of the matches; if no matches are found, it returns nil."
   [companies]

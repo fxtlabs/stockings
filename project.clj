3c95bc1fc07e38480157522d9667dd43368988c4
Koen Van der Auwera <koen@atog.be>
2012-10-04 11:23:06 +0200

    update version nr

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.fxtlabs/stockings "1.1.0-SNAPSHOT"
+(defproject com.fxtlabs/stockings "1.2.0-SNAPSHOT"
   :description "Easy access to financial data: stock quotes, exchange rates, industry sectors, companies, and more."
   :url "https://github.com/fxtlabs/stockings"
   :dependencies [[org.clojure/clojure "1.4.0"]
